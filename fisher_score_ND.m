41c11f112476a6455252c15cb29a6ce53022f2bd
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-01-30 18:04:51 +0330

    fisher scores (2D/ND) added

---
+++ fisher_score_ND.m
@@ -0,0 +1,22 @@
+function J = fisher_score_ND(sigg_cls1, sigg_cls2, all_sub)
+
+    mu_0 = mean(all_sub')'
+    mu_1 = mean(sigg_cls1')'
+    mu_2 = mean(sigg_cls2')'
+    S1 = 0;
+    S2 = 0;
+
+    for sub1 = 1:size(sigg_cls1, 2)
+        S1 = S1+(sigg_cls1(:,sub1)-mu_1)*(sigg_cls1(:,sub1)-mu_1)';
+    end
+    S1 = S1/sub1;
+
+    for sub2 = 1:size(sigg_cls2, 2)
+        S2 = (sigg_cls2(:,sub2)-mu_2)*(sigg_cls2(:,sub2)-mu_2)';
+    end
+    S2 = S2/sub2;
+
+    Sw = S1+S2;
+    Sb = ((mu_1-mu_0)*(mu_1-mu_0)'+(mu_2-mu_0)*(mu_2-mu_0)');
+
+    J = trace(Sb)/trace(Sw);
--- main.m
+++ main.m
@@ -4,20 +4,55 @@ load Datas/All_data.mat;
 %% Configs
 clc
 fs = 100;
-num_features = 10;
+num_features = 13;
 X = x_train;
-[~, cls0_indexes] = find(y_train==0);
-[~, cls1_indexes] = find(y_train==1);
+[~, cls1_indexes] = find(y_train==0);
+[~, cls2_indexes] = find(y_train==1);
 %% Feature Extraction
 clc
 features = feature_extraction(X, num_features, fs);
-%% Calculating the Fisher Score
+%% Calculating the 2D Fisher Score
 clc
-FS = zeros(no_feature, size(features,3));
+FS = zeros(num_features, size(features,3));
 for ch = 1:size(features,3)
-    for f = 1:no_feature
-       sigg_cls0 = features(f, cls0_indexes, ch);
+    for f = 1:num_features
        sigg_cls1 = features(f, cls1_indexes, ch);
-       FS(f, ch) = fisher_score(sigg_cls0, sigg_cls1, features(f, :, ch));
+       sigg_cls2 = features(f, cls2_indexes, ch);
+       FS(f, ch) = fisher_score_2D(sigg_cls1, sigg_cls2, features(f, :, ch));
     end
 end
+
+mean_fisher = mean(FS, 'all');
+var_fisher = var(FS, 0 , 'all');
+thresh = 2*mean_fisher;
+thresh = 0.01;
+[row, col] = find(FS>thresh);
+selected_features = zeros(length(row), size(features,2));
+for i = 1:length(row)
+    selected_features(i,:) = features(row(i), :, col(i));
+end
+selected_features = cell2mat(arrayfun(@(x) features(row(x), :, col(x)), 1:length(row),'UniformOutput',false)');
+
+%% Calculating the ND Fisher Score
+clc
+k = 12;
+num_itters = 100;
+selected_samples_memory = zeros(num_itters, k);
+FS_ND = zeros(1,100);
+for itter = 1:num_itters
+   samples_feature_indexes = randsample(1:size(selected_features, 1), k);
+   selected_samples_memory(itter, :) = samples_feature_indexes;
+   sigg_cls1 = selected_features(samples_feature_indexes, cls1_indexes);
+   sigg_cls2 = selected_features(samples_feature_indexes, cls2_indexes);
+   FS_ND(1, itter) = fisher_score_ND(sigg_cls1, sigg_cls2, selected_features(samples_feature_indexes, :));
+end
+
+max(FS_ND)
+
+
+%%
+samples_feature_indexes = [18,12,16,2,35,5,28,7,20,14,25,11];
+selected_samples_memory(itter, :) = samples_feature_indexes;
+sigg_cls1 = selected_features(samples_feature_indexes, cls1_indexes);
+sigg_cls2 = selected_features(samples_feature_indexes, cls2_indexes);
+fisher_score_ND(sigg_cls1, sigg_cls2, selected_features(samples_feature_indexes, :))
