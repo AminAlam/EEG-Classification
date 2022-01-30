23ab780fc8544aa5afbf1a430713b2bec0a25494
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-01-30 11:56:48 +0330

    chaos features added

--- main.m
+++ main.m
@@ -21,11 +21,68 @@ for  i = 1:size(X, 3)
     features(n_f,i,:) = medfreq(x_train(:,:,i),fs);
 end
 
-
-% Med Freq
-n_f = 2;
+% Total Power of Channels
+n_f = 3;
 for  i = 1:size(X, 3)
-    features(n_f,i,:) = medfreq(x_train(:,:,i),fs);
+    features(n_f,i,:) = bandpower(x_train(:,:,i));
 end
+
+% Power of Theta Band
+n_f = 4;
+freq_band = [4, 8];
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = bandpower(x_train(:,:,i), fs, freq_band);
+end
+
+% Power of alpha Band
+n_f = 5;
+freq_band = [8, 12];
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = bandpower(x_train(:,:,i), fs, freq_band);
+end
+
+% Power of Beta Band
+n_f = 6;
+freq_band = [12, 30];
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = bandpower(x_train(:,:,i), fs, freq_band);
+end
+
+% Power of Beta Band
+n_f = 7;
+freq_band = [30, 50];
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = bandpower(x_train(:,:,i), fs, freq_band);
+end
+
+% Entropy
+n_f = 8;
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = entropy(x_train(:,:,i));
+end
+
+% Lyapunov Exponent
+n_f = 9;
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = lyapunovExponent(x_train(:,:,i), fs);
+end
+
+% Correlation Dim
+n_f = 10;
+for  i = 1:size(X, 3)
+    features(n_f,i,:) = correlationDimension(x_train(:,:,i));
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
