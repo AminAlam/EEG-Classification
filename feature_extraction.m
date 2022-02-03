86794a128516eca6d3b092225f72d607bc73c81c
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-02-03 19:22:32 +0330

    new features added

--- feature_extraction.m
+++ feature_extraction.m
@@ -2,61 +2,73 @@ function [features, num_features] = feature_extraction(X, fs)
     features = zeros(100, size(X, 3), size(X, 2));
     
     for  i = 1:size(X, 3)
-        % Mean Freq
+        % 1 Mean Freq
         n_f = 1;
         features(n_f,i,:) = meanfreq(X(:,:,i),fs);
 
-        % Med Freq
+        % 2 Med Freq
         n_f = n_f+1;
         features(n_f,i,:) = medfreq(X(:,:,i),fs);
 
-        % Total Power of Channels
+        % 3 Total Power of Channels
         n_f = n_f+1;
         features(n_f,i,:) = bandpower(X(:,:,i));
-        
-        % Power of Delta Band
-        n_f = n_f+1;
-        freq_band = [0.5, 4];
-        features(n_f,i,:) = bandpower(X(:,:,i), fs, freq_band);
 
-        % Power of Theta Band
-        n_f = n_f+1;
-        freq_band = [4, 8];
-        features(n_f,i,:) = bandpower(X(:,:,i), fs, freq_band);
-
-        % Power of alpha Band
+        % 4 Power of 8-12
         n_f = n_f+1;
         freq_band = [8, 12];
         features(n_f,i,:) = bandpower(X(:,:,i), fs, freq_band);
 
-        % Power of Beta Band
+        % 5 Power of 12-16
         n_f = n_f+1;
-        freq_band = [12, 35];
+        freq_band = [12, 16];
+        features(n_f,i,:) = bandpower(X(:,:,i), fs, freq_band);
+
+        % 6 Power of 16-20
+        n_f = n_f+1;
+        freq_band = [16, 20];
+        features(n_f,i,:) = bandpower(X(:,:,i), fs, freq_band);
+
+        % 7 Power of 20-24
+        n_f = n_f+1;
+        freq_band = [20, 24];
         features(n_f,i,:) = bandpower(X(:,:,i), fs, freq_band);
 
         for j = 1:size(X, 2)
-            % Entropy
+            % 8 Entropy
             n_f_tmp = n_f+1;
             features(n_f_tmp,i,j) = entropy(X(:,j,i));
-       
-            % Lyapunov Exponent
+
+            % 9 Wavelet Packet Shannon Entropy
+            n_f_tmp = n_f_tmp+1;
+            features(n_f_tmp,i,j) = wentropy(X(:,j,i),'shannon');
+
+            % 10 Wavelet Packet Log Energy Entropy
+            n_f_tmp = n_f_tmp+1;
+            features(n_f_tmp,i,j) = wentropy(X(:,j,i),'log energy');
+
+            % 11 Lyapunov Exponent
             n_f_tmp = n_f_tmp+1;
             features(n_f_tmp,i,j) = lyapunovExponent(X(:,j,i), fs);
-        
+
             % Correlation Dim
-%             n_f = n_f+1;
-%             features(n_f,i,j) = correlationDimension(X(:,j,i));
+            n_f_tmp = n_f_tmp+1;
+            features(n_f_tmp,i,j) = correlationDimension(X(:,j,i));
+            
+            % mean of diff of trial
+            n_f_tmp = n_f_tmp+1;
+            features(n_f_tmp,i,j) = mean(diff(X(:,j,i)));
         end
-        
-        % Skewness
-        n_f = n_f+1;
+
+        % 12 Skewness
+        n_f = n_f_tmp+1;
         features(n_f,i,:) = skewness(X(:,:,i));
-        
-        % Kurtosis
+
+        % 13 Kurtosis
         n_f = n_f+1;
         features(n_f,i,:) = kurtosis(X(:,:,i));
-        
-        % OBW
+
+        % 14 OBW
         n_f = n_f+1;
         features(n_f,i,:) = obw(X(:,:,i), fs);
         
